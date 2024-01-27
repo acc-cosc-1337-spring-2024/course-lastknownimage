8dd6aff735535c6e999537c78c8cd21a4ad56b98
Jovan <118417780+lastknownimage@users.noreply.github.com>
2024-01-27 02:06:44 +0000

    Finished homework 1

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,16 @@
-#
+from output import multiply_numbers
+
+result = multiply_numbers(6,6)
+print(result)
+
+#multiply_numbers()
+
+
+#import input_process_output
+
+#def main():
+#    result = multiply_numbers(6,6)
+#    print(result)
+#    #return result
+
+#main()
