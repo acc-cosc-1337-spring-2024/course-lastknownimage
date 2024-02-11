46adac86988a217fdcb230c0592a43fa136a536e
Jovan <118417780+lastknownimage@users.noreply.github.com>
2024-02-11 05:01:44 +0000

    caught an error mid-commit

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,5 +1,5 @@
 import unittest
 
-rom src.homework.c_decisions import test_decisions
+from src.homework.c_decisions import test_decisions
 class Test_Config(unittest.TestCase):
-suite = unittest.TestLoader().loadTestsFromModule(test_decisions)
+    suite = unittest.TestLoader().loadTestsFromModule(test_decisions)
