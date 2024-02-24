a0b75a75abab0ea407df08b5763c1580fcee3c77
Jovan <118417780+lastknownimage@users.noreply.github.com>
2024-02-24 18:54:38 +0000

    finished

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,9 @@
-import unittest
-'''
-the file in /tests/homework/b_in_proc_out/tests_in_proc_out
-has the test functions
-'''
-from tests.homework.b_in_proc_out import tests_in_proc_out
+from src.homework.e_functions.value_return import get_hour, get_minutes, get_seconds
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
-unittest.TextTestRunner(verbosity=2).run(suite)
+epoch_seconds = 3800
+
+hours = get_hour(epoch_seconds)
+minutes = get_minutes(epoch_seconds)
+seconds = get_seconds(epoch_seconds)
+
+print(f"The time is {hours:02d}:{minutes:02d}:{seconds:02d}")
