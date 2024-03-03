071ee45afe38b171055228f210d2da6c956313e6
Jovan <118417780+lastknownimage@users.noreply.github.com>
2024-03-03 04:44:03 +0000

    Changed return_value to strings, issue is with src file nonexistence

--- tests/homework/h_strings/tests_strings.py
+++ tests/homework/h_strings/tests_strings.py
@@ -1,6 +1,6 @@
 import unittest
-from src.homework.h_strings.value_return import get_hamming_distance
-from src.homework.h_strings.value_return import get_dna_complement
+from src.homework.h_strings.strings import get_hamming_distance
+from src.homework.h_strings.strings import get_dna_complement
 
 class TestConfig(unittest.TestCase):
     def test_get_hamming_distance(self):
