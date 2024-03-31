3279e8e62baf029a3155ec2fa6c64b30c364916f
Jovan <118417780+lastknownimage@users.noreply.github.com>
2024-03-31 03:47:25 +0000

    finished

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,15 @@
-#
+RECORD_DELETED = {'widgets': '', 'quantities': []}
+
+def add_widget(widgets, widget_name, quantity):
+    if not widget_name in widgets:
+        widgets[widget_name] += quantity
+    else:
+        widgets[widget_name] = quantity
+
+def remove_inventory_widget(widget_name, widgets):
+    if widget_name in widgets:
+        RECORD_DELETED.append(widget_name)
+        widgets.pop(widget_name)
+        return RECORD_DELETED
+    else:
+        return 'Item not found'
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,26 @@
-#
+import unittest
+
+from src.homework.i_dictionaries_sets import get_p_distance
+
+class Test_Config(unittest.TestCase):
+
+    def test_add_inventory(self):
+        inventory = {}
+        get_p_distance.add_inventory(inventory, "Widget1", 10)
+        self.assertEqual(inventory["Widget1"], 10) 
+
+        get_p_distance.add_inventory(inventory, "Widget1", 25)
+        self.assertEqual(inventory["Widget1"], 35) 
+
+        get_p_distance.add_inventory(inventory, "Widget1", -10)
+        self.assertEqual(inventory["Widget1"], 25) 
+
+    def test_remove_inventory_widget(self):
+        inventory = {"Widget1": 10, "Widget2": 20}
+        result = get_p_distance.remove_inventory_widget(inventory, "Widget1")
+        self.assertEqual(len(inventory), 1)  
+        self.assertNotIn("Widget1", inventory) 
+        self.assertIn("Widget2", inventory) 
+
+if __name__ == '__main__':
+    unittest.main()
